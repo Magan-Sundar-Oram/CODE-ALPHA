7a9ef3461029f1646e8dbdb2d582b8d627d796aa
Magan Sundar Oram <137607000+Magan-Sundar-Oram@users.noreply.github.com>
2024-01-25 14:27:48 +0530

    Edited script.js

--- weather-app/script.js
+++ weather-app/script.js
@@ -195,7 +195,7 @@ let marker;
 function initMap(lat, lon) {
     mapElement.innerHTML = '';
     if (map) map.remove();
-    map = L.map('map').setView([lat, lon], 13);
+    map = L.map('map').setView([lat, lon], 11);
     marker = L.marker([lat, lon]).addTo(map);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
